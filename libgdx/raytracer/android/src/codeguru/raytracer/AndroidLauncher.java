1ec7a69654351b824242736b51840088275bb099
Code-Apprentice <codeguru42@gmail.com>
2024-01-30 17:47:58 -0700

    LibGDX project for raytracer

---
+++ libgdx/raytracer/android/src/codeguru/raytracer/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package codeguru.raytracer;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import codeguru.raytracer.Raytracer;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Raytracer(), config);
+	}
+}
