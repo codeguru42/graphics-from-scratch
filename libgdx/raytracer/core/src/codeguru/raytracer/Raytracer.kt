422ac2efe898f9d66e58d0090fcc0faaf81d76b1
Code-Apprentice <codeguru42@gmail.com>
2024-01-31 12:41:41 -0700

    Add camera

--- libgdx/raytracer/core/src/codeguru/raytracer/Raytracer.kt
+++ libgdx/raytracer/core/src/codeguru/raytracer/Raytracer.kt
@@ -1,27 +1,23 @@
 package codeguru.raytracer
 
 import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.graphics.Camera
+import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.utils.ScreenUtils
 
 class Raytracer : ApplicationAdapter() {
-    var batch: SpriteBatch? = null
-    var img: Texture? = null
+    private var camera: Camera? = null
+
     override fun create() {
-        batch = SpriteBatch()
-        img = Texture("badlogic.jpg")
+        camera = OrthographicCamera()
     }
 
     override fun render() {
-        ScreenUtils.clear(1f, 0f, 0f, 1f)
-        batch!!.begin()
-        batch!!.draw(img, 0f, 0f)
-        batch!!.end()
+        ScreenUtils.clear(0f, 0f, 0f, 1f)
     }
 
     override fun dispose() {
-        batch!!.dispose()
-        img!!.dispose()
     }
 }
