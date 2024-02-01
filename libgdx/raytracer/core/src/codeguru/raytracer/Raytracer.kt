71ee15466bfbea44fd6d9fb99991732a7571d03c
Code-Apprentice <codeguru42@gmail.com>
2024-01-31 19:33:18 -0700

    Set viewport

--- libgdx/raytracer/core/src/codeguru/raytracer/Raytracer.kt
+++ libgdx/raytracer/core/src/codeguru/raytracer/Raytracer.kt
@@ -3,15 +3,16 @@ package codeguru.raytracer
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.graphics.Camera
 import com.badlogic.gdx.graphics.OrthographicCamera
-import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.utils.ScreenUtils
 
 class Raytracer : ApplicationAdapter() {
+    private val VIEWPORT_WIDTH: Float = 80.0f
+    private val VIEWPORT_HEIGHT: Float = 60.0f
+
     private var camera: Camera? = null
 
     override fun create() {
-        camera = OrthographicCamera()
+        camera = OrthographicCamera(VIEWPORT_WIDTH, VIEWPORT_HEIGHT)
     }
 
     override fun render() {
