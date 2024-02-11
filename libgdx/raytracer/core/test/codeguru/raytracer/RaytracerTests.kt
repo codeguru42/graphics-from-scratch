3de1bb23348a801867f48461dba7e91ad21a83d7
Code-Apprentice <codeguru42@gmail.com>
2024-02-10 21:11:03 -0700

    Setup junit tests

---
+++ libgdx/raytracer/core/test/codeguru/raytracer/RaytracerTests.kt
@@ -0,0 +1,12 @@
+package codeguru.raytracer
+
+import org.junit.Assert
+import kotlin.test.Test
+
+
+class RaytracerTests {
+    @Test
+    fun testFoo() {
+        Assert.assertEquals(1, 1)
+    }
+}
