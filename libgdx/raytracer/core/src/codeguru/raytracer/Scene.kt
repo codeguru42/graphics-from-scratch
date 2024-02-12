a5e4741a56b70a773b7d92c8a6412bb0beb6a12b
Code-Apprentice <codeguru42@gmail.com>
2024-02-11 19:13:11 -0700

    Named arguments

--- libgdx/raytracer/core/src/codeguru/raytracer/Scene.kt
+++ libgdx/raytracer/core/src/codeguru/raytracer/Scene.kt
@@ -4,15 +4,39 @@ import com.badlogic.gdx.graphics.Color
 
 class Scene {
     val spheres: List<Sphere> = listOf(
-        Sphere(Point3(0.0f, -1.0f, 3.0f), 1.0f, Color.RED),
-        Sphere(Point3(2.0f, 0.0f, 4.0f), 1.0f, Color.BLUE),
-        Sphere(Point3(-2.0f, 0.0f, 4.0f), 1.0f, Color.GREEN),
-        Sphere(Point3(0.0f, -5001.0f, 0.0f), 5000.0f, Color.YELLOW),
+        Sphere(
+            center = Point3(x = 0.0f, y = -1.0f, z = 3.0f),
+            radius = 1.0f,
+            color = Color.RED
+        ),
+        Sphere(
+            center = Point3(x = 2.0f, y = 0.0f, z = 4.0f),
+            radius = 1.0f,
+            color = Color.BLUE
+        ),
+        Sphere(
+            center = Point3(-2.0f, 0.0f, 4.0f),
+            radius = 1.0f,
+            color = Color.GREEN
+        ),
+        Sphere(
+            center = Point3(x = 0.0f, y = -5001.0f, z = 0.0f),
+            radius = 5000.0f,
+            color = Color.YELLOW
+        ),
     )
 
     val lights: List<Light> = listOf(
-        AmbientLight(0.2f),
-        PointLight(0.6f, Point3(2.0f, 1.0f, 0.0f)),
-        DirectionalLight(0.2f, Vector(1.0f, 4.0f, 4.0f)),
+        AmbientLight(
+            intensity = 0.2f
+        ),
+        PointLight(
+            intensity = 0.6f,
+            position = Point3(2.0f, 1.0f, 0.0f)
+        ),
+        DirectionalLight(
+            intensity = 0.2f,
+            direction = Vector(1.0f, 4.0f, 4.0f)
+        ),
     )
 }
