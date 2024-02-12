367979d88e5208b46144b1fa364948ca302cfb4c
Code-Apprentice <codeguru42@gmail.com>
2024-02-11 22:20:52 -0700

    Fix test

--- libgdx/raytracer/core/test/codeguru/raytracer/SphereTests.kt
+++ libgdx/raytracer/core/test/codeguru/raytracer/SphereTests.kt
@@ -8,9 +8,9 @@ class SphereTests {
     @Test
     fun testIntersect() {
         val s = Sphere(Point3(-2.0f, 0.0f, 4.0f), 1.0f, Color.GREEN)
-        val p1 = Point3(-2.0f, 0.0f, 0.0f)
-        val p2 = Point3(-2.0f, 0.0f, 1.0f)
-        val actual = s.intersect(p1, p2)
+        val p = Point3(-2.0f, 0.0f, 0.0f)
+        val v = Vector(0.0f, 0.0f, 1.0f)
+        val actual = s.intersect(p, v)
         val expected = Pair(5.0f, 3.0f)
         assertThat(actual).isEqualTo(expected)
     }
